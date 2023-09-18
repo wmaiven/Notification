282c156e1ed75e61b7d655ce45d01155b1dfd38f
wmaiven <maivencunha@hotmail.com>
2023-09-18 14:38:04 -0300

    intalando dependencias

---
+++ index.js
@@ -0,0 +1,28 @@
+// notificationService.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/send-notification', (req, res) => {
+  const employeeIds = req.body.employeeIds;
+  const message = req.body.message;
+
+  if (!employeeIds || !message) {
+    return res.status(400).json({ error: 'Parâmetros inválidos' });
+  }
+
+  // Simulando o envio da notificação
+  employeeIds.forEach(employeeId => {
+    console.log(`Enviando notificação para Employee ID ${employeeId}: ${message}`);
+  });
+
+  return res.status(200).json({ message: 'Notificação enviada com sucesso' });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Serviço de Notificação rodando na porta ${port}`);
+});
